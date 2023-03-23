014c2fee7eb9d299e2063a018d0d928df6a40358
GribUser <grib@gribuser.ru>
2023-03-23 18:08:44 +0300

    Default blank UE project

---
+++ Source/PieMenuDemo/PieMenuDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PieMenuDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APieMenuDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APieMenuDemoGameMode();
+};
+
+
+
