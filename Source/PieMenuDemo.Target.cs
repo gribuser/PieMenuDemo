17225cbee172f60e9c552dfcc68168f47d358779
GribUser <grib@gribuser.ru>
2024-04-26 13:46:53 +0300

    Latest toolchain

--- Source/PieMenuDemo.Target.cs
+++ Source/PieMenuDemo.Target.cs
@@ -8,7 +8,7 @@ public class PieMenuDemoTarget : TargetRules
 	public PieMenuDemoTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		ExtraModuleNames.Add("PieMenuDemo");
 	}
 }
